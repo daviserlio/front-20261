818966c5851e56e90292d1e9d6bf19acef599afb
Davi Lopes <davilopeslinkedin@gmail.com>
2026-04-13 15:43:49 -0300

    docs: pequenos ajustes antes de trocar de branch

--- praticas/aluno-online-vanilla/src/main.js
+++ praticas/aluno-online-vanilla/src/main.js
@@ -1,1 +1,60 @@
-import './style.css'
+import "./style.css";
+
+const form = document.getElementById("loginForm");
+
+if (form) {
+  const email = document.getElementById("email");
+  const senha = document.getElementById("senha");
+
+  const emailError = document.getElementById("emailError");
+  const senhaError = document.getElementById("senhaError");
+
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let valido = true;
+
+    emailError.textContent = "";
+    emailError.style.display = "none";
+    email.classList.remove("input-error");
+
+    senhaError.textContent = "";
+    senhaError.style.display = "none";
+    senha.classList.remove("input-error");
+
+    if (email.value.trim() === "") {
+      emailError.textContent = "O Campo de email é obrigatório";
+      emailError.style.display = "block";
+      email.classList.add("input-error");
+      valido = false;
+    }
+
+
+    if (senha.value.trim() === "") {
+      senhaError.textContent = "O Campo de senha é obrigatório";
+      senhaError.style.display = "block";
+      senha.classList.add("input-error");
+      valido = false;
+    }
+
+    if (valido) {
+      window.location.href = "./index.html";
+    }
+  });
+
+  email.addEventListener("input", () => {
+    if (email.value.trim() !== "") {
+      emailError.textContent = "";
+      emailError.style.display = "none";
+      email.classList.remove("input-error");
+    }
+  });
+
+  senha.addEventListener("input", () => {
+    if (senha.value.trim() !== "") {
+      senhaError.textContent = "";
+      senhaError.style.display = "none";
+      senha.classList.remove("input-error");
+    }
+  });
+}
